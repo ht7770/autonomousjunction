7fb29a71180e16eff008b14091b561c67c0f79b0
ht7770 <58001312+ht7770@users.noreply.github.com>
2021-11-05 16:45:24 +0000

    added left and right functionality to main.py

--- car/main.py
+++ car/main.py
@@ -13,6 +13,18 @@ def motor_backward():
     time.sleep(1)
     PWM.setMotorModel(0, 0, 0, 0)
 
+def motor_left():
+    PWM.setMotorModel(-1000, -1000, 1000, 1000)
+    print("Moving left...")
+    time.sleep(1)
+    PWM.setMotorModel(0, 0, 0, 0)
+
+def motor_right():
+    PWM.setMotorModel(1000, 1000, -1000, -1000)
+    print("Moving right...")
+    time.sleep(1)
+    PWM.setMotorModel(0, 0, 0, 0)
+
 
 if __name__ == '__main__':
 
@@ -22,4 +34,9 @@ if __name__ == '__main__':
         motor_forward()
     elif sys.argv[1] == 'backward':
         motor_backward()
+    elif sys.argv[1] == 'left':
+        motor_left()
+    elif sys.argv[1] == 'right':
+        motor_right()
+
 
