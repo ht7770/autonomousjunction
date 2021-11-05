f0ebbd3365086e1cd783b0cd08bd7f068205894d
ht7770 <58001312+ht7770@users.noreply.github.com>
2021-11-05 16:32:55 +0000

    Swapped motor values so that forwards and backwards now correspond to actual movement of car
    also added backwards functionality to car

--- car/main.py
+++ car/main.py
@@ -7,10 +7,19 @@ def motor_forward():
     PWM.setMotorModel(0,0,0,0)
 
 
+def motor_backward():
+    PWM.setMotorModel(-1000, -1000, -1000, -1000)
+    print("Moving backwards...")
+    time.sleep(1)
+    PWM.setMotorModel(0, 0, 0, 0)
+
+
 if __name__ == '__main__':
 
     print("Starting...")
     import sys
-    if sys.argv[1] == 'Motor':
+    if sys.argv[1] == 'forward':
         motor_forward()
+    elif sys.argv[1] == 'backward':
+        motor_backward()
 
