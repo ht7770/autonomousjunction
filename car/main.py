9f45f1b29886ddfa886b23b78ffe4cba2176a40e
ht7770 <58001312+ht7770@users.noreply.github.com>
2021-11-05 16:27:13 +0000

    Added support for user to call function to main.py

--- car/main.py
+++ car/main.py
@@ -4,4 +4,13 @@ def motor_forward():
     PWM.setMotorModel(1000, 1000, 1000, 1000)
     print("Moving forwards...")
     time.sleep(1)
+    PWM.setMotorModel(0,0,0,0)
+
+
+if __name__ == '__main__':
+
+    print("Starting...")
+    import sys
+    if sys.argv[1] == 'Motor':
+        motor_forward()
 
