7cdb241c5ade248be5f55f6ff0e469c7494206ef
ht7770 <58001312+ht7770@users.noreply.github.com>
2021-11-05 17:00:34 +0000

    added ability to change time motors are on for

--- car/main.py
+++ car/main.py
@@ -30,8 +30,9 @@ def motor_test():
     leftrear = int(input("Enter value for left rear motor: "))
     rightfront = int(input("Enter value for right front motor: "))
     rightrear = int(input("Enter value for right rear motor: "))
+    time = int(input("Enter a time till motor shutoff: "))
     PWM.setMotorModel(leftfront, leftrear, rightfront, rightrear)
-    time.sleep(3)
+    time.sleep(time)
     PWM.setMotorModel(0,0,0,0)
 
 
