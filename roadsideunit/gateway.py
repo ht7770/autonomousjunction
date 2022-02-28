a065cce96c66b3810d3a692fffc517c31f9befc5
ht7770 <58001312+ht7770@users.noreply.github.com>
2022-02-28 19:49:47 +0000

    bugfix

--- roadsideunit/gateway.py
+++ roadsideunit/gateway.py
@@ -154,81 +154,80 @@ def main():
 
     client = createMQTT(projectID, cloudRegion, registryID, gatewayID, private_key_file, algorithm, certificateFile, gateway.mqtt_bridge_hostname, gateway.mqtt_bridge_port, JWTexpire)
 
-    client.loop()
-    print("TEST")
-    if gateway.connected is False:
-        print('connect status {}'.format(gateway.connected))
-        time.sleep(1)
-        continue
+    while True:
+        client.loop()
+        print("TEST")
+        if gateway.connected is False:
+            print('connect status {}'.format(gateway.connected))
+            time.sleep(1)
+            continue
 
-    try:
-        data, client_addr = udpSerSock.recvfrom(BUFSIZE)
-    except socket.error:
-        continue
-    print('[{}]: From Address {}:{} receive data: {}'.format(
-        ctime(), client_addr[0], client_addr[1], data.decode("utf-8")))
+        try:
+            data, client_addr = udpSerSock.recvfrom(BUFSIZE)
+        except socket.error:
+            continue
+        print('[{}]: From Address {}:{} receive data: {}'.format(
+            time.ctime(), client_addr[0], client_addr[1], data.decode("utf-8")))
 
-    command = json.loads(data.decode('utf-8'))
-    if not command:
-        print('invalid json command {}'.format(data))
-        continue
+        command = json.loads(data.decode('utf-8'))
+        if not command:
+            print('invalid json command {}'.format(data))
+            continue
 
-    action = command["action"]
-    device_id = command["device"]
-    template = '{{ "device": "{}", "command": "{}", "status" : "ok" }}'
+        action = command["action"]
+        device_id = command["device"]
+        template = '{{ "device": "{}", "command": "{}", "status" : "ok" }}'
 
-    if action == 'event':
-        print('Sending telemetry event for device {}'.format(device_id))
-        payload = command["data"]
+        if action == 'event':
+            print('Sending telemetry event for device {}'.format(device_id))
+            payload = command["data"]
 
-        mqtt_topic = '/devices/{}/events'.format(device_id)
-        print('Publishing message to topic {} with payload \'{}\''.format(
-            mqtt_topic, payload))
-        _, event_mid = client.publish(mqtt_topic, payload, qos=0)
+            mqtt_topic = '/devices/{}/events'.format(device_id)
+            print('Publishing message to topic {} with payload \'{}\''.format(
+                mqtt_topic, payload))
+            _, event_mid = client.publish(mqtt_topic, payload, qos=0)
 
-        message = template.format(device_id, 'event')
-        udpSerSock.sendto(message.encode('utf8'), client_addr)
+            message = template.format(device_id, 'event')
+            udpSerSock.sendto(message.encode('utf8'), client_addr)
 
-    elif action == 'attach':
-        print('Sending telemetry event for device {}'.format(device_id))
-        attach_topic = '/devices/{}/attach'.format(device_id)
-        auth = ''  # TODO:    auth = command["jwt"]
-        attach_payload = '{{"authorization" : "{}"}}'.format(auth)
+        elif action == 'attach':
+            print('Sending telemetry event for device {}'.format(device_id))
+            attach_topic = '/devices/{}/attach'.format(device_id)
+            auth = ''  # TODO:    auth = command["jwt"]
+            attach_payload = '{{"authorization" : "{}"}}'.format(auth)
 
-        print('Attaching device {}'.format(device_id))
-        print(attach_topic)
-        response, attach_mid = client.publish(
-            attach_topic, attach_payload, qos=1)
+            print('Attaching device {}'.format(device_id))
+            print(attach_topic)
+            response, attach_mid = client.publish(
+                attach_topic, attach_payload, qos=1)
 
-        message = template.format(device_id, 'attach')
-        udpSerSock.sendto(message.encode('utf8'), client_addr)
-    elif action == 'detach':
-        detach_topic = '/devices/{}/detach'.format(device_id)
-        print(detach_topic)
+            message = template.format(device_id, 'attach')
+            udpSerSock.sendto(message.encode('utf8'), client_addr)
+        elif action == 'detach':
+            detach_topic = '/devices/{}/detach'.format(device_id)
+            print(detach_topic)
 
-        res, mid = client.publish(detach_topic, "{}", qos=1)
+            res, mid = client.publish(detach_topic, "{}", qos=1)
 
-        message = template.format(res, mid)
-        print('sending data over UDP {} {}'.format(client_addr, message))
-        udpSerSock.sendto(message.encode('utf8'), client_addr)
+            message = template.format(res, mid)
+            print('sending data over UDP {} {}'.format(client_addr, message))
+            udpSerSock.sendto(message.encode('utf8'), client_addr)
 
-    elif action == "subscribe":
-        print('subscribe config for {}'.format(device_id))
-        subscribe_topic = '/devices/{}/config'.format(device_id)
-        skip_next_sub = True
+        elif action == "subscribe":
+            print('subscribe config for {}'.format(device_id))
+            subscribe_topic = '/devices/{}/config'.format(device_id)
+            skip_next_sub = True
 
-        _, mid = client.subscribe(subscribe_topic, qos=1)
-        message = template.format(device_id, 'subscribe')
-        gateway_state.subscriptions[subscribe_topic] = client_addr
+            _, mid = client.subscribe(subscribe_topic, qos=1)
+            message = template.format(device_id, 'subscribe')
+            gateway.subscriptions[subscribe_topic] = client_addr
 
-        udpSerSock.sendto(message.encode('utf8'), client_addr)
+            udpSerSock.sendto(message.encode('utf8'), client_addr)
 
-    else:
-        print('undefined action: {}'.format(action))
+        else:
+            print('undefined action: {}'.format(action))
 
-
-print('Finished.')
-
+    print('Finished.')
 
 
 if __name__ == '__main__':
